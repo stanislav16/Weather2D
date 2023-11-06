30bec3348c914aea71a00be97de27919fa0f2fc3
stanislav16 <sormanliev@gmail.com>
2023-11-06 18:43:38 +0200

    trying to make the openweather api work and trying to get the hourly forecast

--- api.js
+++ api.js
@@ -1,29 +1,55 @@
 let cityEl;
 let city;
 let button = document.getElementById('value');
+let lat;
+let lon;
 
 // API Key
-let api = '6fb7b0bff001cb28ebf17743592d0cca';
+let api = 'ea73d5e89224d63f1f7c52734328a954';
 
-
-function getWeatherForDay(city) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api}`)
+function getCityCoords(city) {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=&appid=${api}`)
         .then(response => {
             return response.json();
         })
         .then(data => {
-            let day = data.list[0];
-                let temp = Math.floor(day.main.temp - 273.15);
-                document.getElementById(`temperature`).innerHTML = `${temp}°C`;
-                document.getElementById(`description`).innerHTML = day.weather[0].main;
-                let iconCode = day.weather[0].icon;
+             lat = data[0].lat;
+             lon = data[0].lon;
+            document.getElementById('kur').innerHTML = lat + ' ' + lon;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+function getWeatherForDay(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${api}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            let temp = Math.floor(data.current.temp - 273.15);
+            document.getElementById(`temperature`).innerHTML = `${temp}°C`;
+            document.getElementById(`description`).innerHTML = data.current.clouds;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
 
-                // Create the URL for the icon image
-                let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-
-                // Set the source of the image element to the icon URL
-                document.getElementById(`icon`).src = iconUrl;
-
+function hourlyTemperature(city){
+    fetch(`https://api.openweathermap.org/data/2.5/forecast/hourly?q=${city}&appid=${api}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);  // Log the data to the console
+            let hourlyForecast = data.list.slice(0, 24); // Get the forecast for the next 24 hours
+            hourlyForecast.forEach((hour, index) => {
+                let temp = Math.floor(hour.main.temp - 273.15);
+                let tempElement = document.createElement('p');
+                tempElement.innerHTML = `Hour ${index}: ${temp}°C`;
+                document.getElementById('hourly').appendChild(tempElement);
+            });
         })
         .catch(err => {
             console.log(err);
@@ -42,12 +68,6 @@ function getWeatherForNextDays(city) {
                 document.getElementById(`temperature${index + 2}`).innerHTML = `${temp}°C`;
                 document.getElementById(`description${index + 2}`).innerHTML = day.weather[0].main;
                 let iconCode = day.weather[0].icon;
-
-                // Create the URL for the icon image
-                let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-
-                // Set the source of the image element to the icon URL
-                document.getElementById(`icon${index}`).src = iconUrl;
             });
         })
         .catch(err => {
@@ -59,7 +79,10 @@ button.addEventListener('click', function() {
     // Get the city from the textbox
     cityEl = document.getElementById('cityBox');
     city = cityEl.value;
-    getWeatherForDay(city);
-    getWeatherForNextDays(city)
+
+    getCityCoords(city)
+         //   getWeatherForDay(lat, lon);
+            getWeatherForNextDays(city);
+            hourlyTemperature(city);
 });
 
