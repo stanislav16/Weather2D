cfc0bb59aabcfdcb8dbd2ef7204fad31f8b08d97
stanislav16 <sormanliev@gmail.com>
2023-11-10 11:22:47 +0200

    making style and js changes

--- api.js
+++ api.js
@@ -4,6 +4,14 @@ let button = document.getElementById('value');
 let lat;
 let lon;
 
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.getElementById('date').innerHTML = today;
+
 // API Key
 let api = '77442ded850fd1a075869d12438a5415';
 
@@ -27,7 +35,7 @@ function getWeatherForDay(lat, lon) {
         })
         .then(data => {
             let temp = Math.floor(data.hourly[0].temp - 273.15);
-            document.getElementById(`temperature`).innerHTML = `${temp}°C`;
+            document.getElementById(`temperature`).innerHTML = `${temp}°`;
         })
         .catch(err => {
             console.log(err);
@@ -35,23 +43,7 @@ function getWeatherForDay(lat, lon) {
 }
 
 // Get Weather
-function getWeatherForNextDays(city) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api}`)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            let forecast = data.list.slice(1, 5); // Get the forecast for the next 4 days
-            forecast.forEach((day, index) => {
-                let temp = Math.floor(day.main.temp - 273.15);
-                document.getElementById(`temperature${index + 2}`).innerHTML = `${temp}°C`;
-                document.getElementById(`description${index + 2}`).innerHTML = day.weather[0].main;
-            });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-}
+
 
 button.addEventListener('click', () => {
     // Get the city from the textbox
