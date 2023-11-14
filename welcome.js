b972e6a0e01f5699d152861c5abb18c3ddb68cd2
stanislav16 <sormanliev@gmail.com>
2023-11-14 21:59:41 +0200

    finalizing project

--- welcome.js
+++ welcome.js
@@ -2,14 +2,14 @@ let cityW;
 var cityWelcome;
 let buttonW = document.getElementById('getCity');
 
-var Test="test";
+var Test = "test";
 
 buttonW.addEventListener('click', () => {
     // Get the city from the textbox
     cityW = document.getElementById('cityWBox');
     cityWelcome = cityW.value;
-    localStorage.setItem("cityWel",cityWelcome);
-    window.location.href="mainPage.html";
+    localStorage.setItem("cityWel", cityWelcome);
+    window.location.href = "mainPage.html";
     console.log(cityWelcome);
 
 
